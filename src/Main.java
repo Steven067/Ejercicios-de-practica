b03f41d1d4a819b205993df8e0e03bb8a1a4acdf
Alejandro <stevenarenas74@gmail.com>
2025-07-07 19:54:26 -0500

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte edad = 18;
+        int numeroGrande = 1000;
+        float decimal = 3.14f;
+        boolean mayorEdad = true;
+        String texto = "Hola  Mundo";
+
+
+        System.out.println(numeroGrande + " Numero Grandes");
+        System.out.println(decimal + " Numero decimal");
+        System.out.println(edad + " Numero pequeños");
+        System.out.println(mayorEdad + " Muestra verdadero o falso");
+        System.out.println(texto);
+
+    }
+}
