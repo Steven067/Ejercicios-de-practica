ec5a054a7f4979bb245efadfa0284cc19b99bdfc
Alejandro <stevenarenas74@gmail.com>
2025-07-24 18:29:16 -0500

    Ejercicio 2: Calcular el precio de un producto con IVA y mostrar solo dos decimales

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,32 @@ public class Main {
 
          */
 
-        Scanner sn = new Scanner (System.in);
 
-        System.out.println("Ingresa tu nombre: ");
-        String nombre = sn.nextLine();
 
-        System.out.println("¡Hola " + nombre + "!");
+       // Scanner sn = new Scanner (System.in);
+
+       // System.out.println("Ingresa tu nombre: ");
+        //String nombre = sn.nextLine();
+
+        //System.out.println("¡Hola " + nombre + "!");
+
+
+        /*  EJERCIOCIO N°2
+
+            Pide el precio de un producto sin iva y calcula su precio con iva
+            EL IVA ES DEL 21%
+            Formatea el resultado para que se muestre con dos decimales
+
+        */
+
+     final double IVA = 0.21;
+
+    Scanner sn = new Scanner(System.in);
+    System.out.println("Ingrese el Precio del producto: ");
+    double producto = sn.nextDouble();
+    double iva = producto * IVA;
+    double precioFinal =  producto + iva;
+    System.out.printf("El precio de su producto es: %.2f ", precioFinal);
 
     }
 }
